3586161ff8d37686e655c8656c5a90be11bde716
Stephen Rasku <mercurial@srasku.net>
2021-05-08 13:07:19 -0700

    Add DOCKER_BUILDKIT to `docker build` command
    This will display the logs of the build so that they can be debugged.

--- in-container.sh
+++ in-container.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 IMAGE=srasku-dev:latest
-docker build -f Docker/Dockerfile -t ${IMAGE} .
+DOCKER_BUILDKIT=0 docker build -f Docker/Dockerfile -t ${IMAGE} .
 docker run -v "$(pwd):/home/develop" --rm ${IMAGE} "$@"
