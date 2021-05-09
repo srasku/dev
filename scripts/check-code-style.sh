1c928050bd3ef447762b8a80624d5321598ca63f
Stephen Rasku <mercurial@srasku.net>
2021-05-08 19:17:06 -0700

    Disable SC2086 for whole file
    Shellcheck warning SC2086 warns about un-quoted variables.  Many of
    these calls contain lists of files to be used as arguments to command
    line programs.  If we quote them they will appear as a single argument
    and the files will not get processed correctly.

--- scripts/check-code-style.sh
+++ scripts/check-code-style.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -e
+# We want each file to be a separate argument below so we allow unquoted
+# variables.
+# shellcheck disable=SC2086
 
 SCM_DIR=$(dirname "$0")/scm/hg
 SH_FILES=$("${SCM_DIR}/ls-files.sh" | grep -v '^?' | grep '\.sh$' | cut -d' ' -f2- | tr '\n' ' ')
 
-# We want each file to be a separate argument so we don't quote SH_FILES
-# shellcheck disable=SC2086
 shellcheck ${SH_FILES}
