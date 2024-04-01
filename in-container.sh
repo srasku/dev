5406ba7b9a1a7fb04d68a0b880edb8f8fd6e69ad
Stephen Rasku <mercurial@srasku.net>
2024-04-01 16:54:29 -0700

    Build container from Docker directory
    This will reduce the amount of context that needs to be sent to Docker
    when building.  The current directory could have a bunch of source code,
    built binaries, etc. that are used for the current project so it's
    faster to omit it.  Any files that need to be installed into the
    container should be placed in the Docker directory or below.

--- in-container.sh
+++ in-container.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
 IMAGE=srasku-dev:latest
-DOCKER_BUILDKIT=0 docker build -f Docker/Dockerfile -t ${IMAGE} .
+DOCKER_BUILDKIT=0 docker build -f Docker/Dockerfile -t ${IMAGE} Docker
 docker run -it -v "$(pwd):/home/develop" --rm ${IMAGE} "$@"
